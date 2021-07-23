730adc1cb5e947029a69abafdd03c23fdad66efd
MrEscape <mrescape@gmail.com>
2021-07-23 14:46:28 -0300

    phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from "react";
+import "./index.css";
 
 import personService from "./services/persons";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import SuccessMessage from "./components/SuccessMessage";
+import ErrorMessage from "./components/ErrorMessage";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     personService.getAll().then(personsData => setPersons(personsData));
@@ -23,41 +28,73 @@ const App = () => {
   const filterContacts = filter === "" ? persons : persons.filter(x => x.name.toLowerCase().includes(filter));
 
   const handleDeletePerson = id => {
-    if (window.confirm(`Delete ${persons.find(person => person.id === id).name} `)) {
-      personService.remove(id).then(res => setPersons(persons.filter(person => person.id !== res)));
+    if (window.confirm(`Delete ${persons.find(person => person.id === id).name}?`)) {
+      personService
+        .remove(id)
+        .then(res => {
+          setSuccessMessage(`${persons.find(person => person.id === id).name} removed`);
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 3000);
+          setPersons(persons.filter(person => person.id !== res));
+        })
+        .catch(error => {
+          setErrorMessage(error);
+        });
     }
   };
 
   const addPerson = e => {
     e.preventDefault();
+
+    const newPersonObject = {
+      name: newName,
+      number: newNumber,
+    };
     // Returns true if the element is in the array
     if (persons.some(person => person.name === newName)) {
-      if (window.confirm(`${newName} is already added to phonebook, replace the new number woth yhe new one?`)) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the new number with the new one?`)) {
         const id = persons.find(person => person.name === newName).id;
-        const newPersonObject = {
-          name: newName,
-          number: newNumber,
-        };
         personService
           .update(newPersonObject, id)
-          .then(response => setPersons(persons.map(person => (person.id !== id ? person : response))));
-        setNewName("");
-        setNewNumber("");
+          .then(response => {
+            setPersons(persons.map(person => (person.id !== id ? person : response)));
+            setSuccessMessage(`${newName} was updated successfully`);
+            setTimeout(() => {
+              setSuccessMessage(null);
+            }, 3000);
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch(error => {
+            console.error(error.message);
+            setErrorMessage(`${newName} was already removed from server`);
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 3000);
+          });
       }
     } else {
-      const newPersonObject = {
-        name: newName,
-        number: newNumber,
-      };
-      personService.create(newPersonObject).then(response => setPersons(persons.concat(response)));
-      setNewName("");
-      setNewNumber("");
+      personService
+        .create(newPersonObject)
+        .then(response => {
+          setPersons(persons.concat(response));
+          setSuccessMessage(`${newName} was added successfully`);
+          setTimeout(() => {
+            setSuccessMessage(null);
+          }, 3000);
+          setNewName("");
+          setNewNumber("");
+        })
+        .catch(error => setErrorMessage(error));
     }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <SuccessMessage message={successMessage} />
+      <ErrorMessage message={errorMessage} />
       <Filter handleFilter={handleFilter} filter={filter} />
       <h3>Add a new Contact</h3>
       <PersonForm
